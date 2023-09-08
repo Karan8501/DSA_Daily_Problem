e8181ad378d157062e8a1e64e67fecb8ad1d0833
unknown <kc80465@gmail.com>
2023-09-08 17:51:41 +0530

    day1

---
+++ src/day1.ts
@@ -0,0 +1,14 @@
+// type informe or fiqure out
+let a = 12;
+let b ='6';
+let c = 2;
+
+// console.log(a/b)
+// it is an valid java script 
+// But ts is stringly type language so is fogure out that b must be type of number of bigint or any
+
+// console.log(b*c)
+
+const arr = [{
+    name:"karan"
+}]
---
+++ src/main.ts
@@ -0,0 +1,28 @@
+// let username = 'karan'
+// let myname:string
+// console.log(username)
+// let a:number= 12;
+// let b:number  = 3;
+// let c:number= 2;
+// console.log(a/b);
+
+
+// console.log(c*b)
+
+// // any type data
+// let data:any;
+// data= "karan";
+// console.log(data);
+// data= 7;
+// console.log(data)
+
+
+// // function in the type script 
+// const sum = (a:number,b:string)=>{
+//     return a+b
+// }
+
+
+// // unian type which holds more tahn one type 
+// let rest:string|number;
+// rest = "karan"
